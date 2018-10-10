c1ee01b25e1ce72339b08eccbbd892dba67ee661
Jagroop Singh <jagroop.singh@kindlebit.com>
2018-10-10 18:50:56 +0530

    emial update

--- resources/views/email/status_reminder.blade.php
+++ resources/views/email/status_reminder.blade.php
@@ -2,6 +2,12 @@
 # Reminder
 
 Please update your tasks progress on Portal before 6:59 PM , Project status will be automatically posted to Mr. Ram Sharma
+
+Things needs to be updated:
+
+- Task status
+- Task Completion Percentage
+
 <br>
 <br>
 Thanks,<br>
