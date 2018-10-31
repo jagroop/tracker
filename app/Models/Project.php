0f997c34a7da5b74dee71b8cfaf0bb435549c3c5
Jagroop Singh <jagroop.singh@kindlebit.com>
2018-10-31 14:02:40 +0530

    work hours changes

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -62,8 +62,9 @@ class Project extends Model implements HasMedia
 
     public function workHours($today = false)
     {
+      $sumBy = ($today) ? 'work_hours' : 'total_work_hours'
       return $this->tasks()->when($today, function($q){
         return $q->whereDate('created_at', now()->toDateString());
-      })->sum('work_hours');
+      })->sum($sumBy);
     }
 }
